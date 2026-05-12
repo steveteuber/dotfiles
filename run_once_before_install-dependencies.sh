78cd74a9e1b43a0bf908ab1cf409977231b8e987
Steve Teuber <github@steveteuber.com>
2026-05-12 14:34:53 +0200

    Add reminder to configure 1Password after installation

--- run_once_before_install-dependencies.sh
+++ run_once_before_install-dependencies.sh
@@ -8,6 +8,9 @@ fi
 if [[ ! -x $(command -v op) ]]; then
   echo "Installing 1Password..."
   brew install --cask 1password 1password-cli
+  # TODO!!!
+  # Open 1Password, sign in all necessary accounts and don't forget
+  # to enable the 1Password CLI in the settings under "Developer"...
 fi
 
 if [[ ! -d ~/.oh-my-zsh ]]; then
